6503e14706ed07df7d7fea22f13b90e3eb4615d7
Alexander Kondratskiy <kholdstare0.0@gmail.com>
2012-09-01 13:06:32 -0400

    Combining javascript files into one for less http requests.

---
+++ javascripts/foundation/join.bash
@@ -0,0 +1,2 @@
+#!/bin/bash
+cat jquery.js jquery.foundation.tooltips.js jquery.foundation.tabs.js jquery.foundation.orbit.js jquery.placeholder.js jquery.foundation.buttons.js jquery.foundation.mediaQueryToggle.js jquery.foundation.alerts.js jquery.foundation.navigation.js jquery.foundation.reveal.js jquery.foundation.forms.js jquery.foundation.accordion.js > jquery.joined.js
